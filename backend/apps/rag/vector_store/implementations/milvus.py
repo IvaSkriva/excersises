atptour2024/openwebui

feat: implement interface for multiple vectorstores with usage of Langchain classes

backend/apps/rag/vector_store/implementations/milvus.py
@@ -1,0 +1,89 @@
+from datetime import datetime
+from typing import Any, Type
+
+from apps.rag.vector_store.vector_store_extension import VectorStoreExtension
+from config import MILVUS_CONNECTION_URI
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+from langchain_milvus import Milvus as LangChainMilvus
+
+
+class Milvus(LangChainMilvus, VectorStoreExtension):
+    """
+    Milvus Interface that overrides Milvus PGVector Class with standardized attributes.
+
+    Args:
+        embedding_function (langchain_core.embeddings): Embedding Function with
+            LangChain interface.
+        collection_name (str): The collection name to use.
+        overwrite_collection (bool): Whether to overwrite current collection.
+        connection_args (dict[str, any]]): The connection args used for
+            this class comes in the form of a dict.
+        **kwargs (Any): Specific attributes linked to PGVector LangChain Class.
+    """
+
+    def __init__(
+        self,
+        embedding_function: Embeddings,
+        collection_name: str = "default",
+        overwrite_collection: bool = False,
+        connection_args: dict[str, any] = {"uri": MILVUS_CONNECTION_URI},
+        **kwargs: Any,
+    ):
+        self._overwrite_collection = overwrite_collection
+        self.collection_name = collection_name
+        self.embedding_function = embedding_function
+        # Avoid conflict of the same attribute appearing in kwargs with
+        # default value and what we add in the super().__init__ below
+        if "drop_old" in kwargs:
+            kwargs.pop("drop_old")
+        super().__init__(
+            collection_name=self.collection_name,
+            embedding_function=self.embedding_function,
+            drop_old=self._overwrite_collection,
+            connection_args=connection_args,
+            **kwargs,
+        )
+
+    def delete_collection(self) -> None:
+        """Delete collection."""
+        return self.col.drop()
+
+    @classmethod
+    def from_documents(
+        cls: Type[LangChainMilvus],
+        documents: list[Document],
+        embedding: Embeddings,
+        ## Modified args
+        connection_args: dict[str, any] = {"uri": MILVUS_CONNECTION_URI},
+        additional_metadata: dict = {},
+        ##
+        **kwargs: Any,
+    ) -> LangChainMilvus:
+        """Return VectorStore initialized from documents and embeddings.
+        This is a copy of the class method from_documents of LangChain Milvus
+        but with additional processes.
+
+        Args:
+            documents: List of Documents to add to the vectorstore.
+            embedding: Embedding function to use.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            VectorStore: VectorStore initialized from documents and embeddings.
+        """
+        texts = [d.page_content for d in documents]
+        metadatas = [{**d.metadata, **additional_metadata} for d in documents]
+        # Vectorstores do not like datetime formats
+        # for meta-data so convert them to string.
+        for metadata in metadatas:
+            for key, value in metadata.items():
+                if isinstance(value, datetime):
+                    metadata[key] = str(value)
+        return cls.from_texts(
+            texts,
+            embedding,
+            metadatas=metadatas,
+            connection_args=connection_args,
+            **kwargs,
+        )

backend/apps/rag/vector_store/vector_store_connector.py
@@ -1,0 +1,55 @@
+from typing import Any, Type
+
+from apps.rag.vector_store.implementations import (
+    Chroma,
+    Milvus,
+    PersistentChroma,
+    PGVector,
+)
+from apps.rag.vector_store.vector_store_extension import VectorStoreExtension
+from config import VECTOR_STORE_TYPE
+from langchain_core.embeddings import Embeddings
+
+
+class VectorStoreConnector:
+    """VectorStoreConnector class permits to use different VectorStores
+    by providing the VectorStore Type."""
+
+    _vector_stores = {
+        "chroma": Chroma,
+        "persistent_chroma": PersistentChroma,
+        "milvus": Milvus,
+        "pgvector": PGVector,
+        "default": PersistentChroma,
+    }
+
+    def __init__(self, vector_store_type: str = "default"):
+        if vector_store_type in self._vector_stores:
+            self.vector_store_type = vector_store_type
+        else:
+            raise ValueError(f"Unknown VectorStore type: {vector_store_type}")
+
+    def get_vs_collection(
+        self,
+        collection_name: str,
+        embedding_function: Embeddings,
+        overwrite_collection: bool = False,
+        **kwargs: Any,
+    ) -> VectorStoreExtension:
+        """Set VectorStore with the proper collection to be used."""
+        return self._vector_stores[self.vector_store_type](
+            collection_name=collection_name,
+            embedding_function=embedding_function,
+            overwrite_collection=overwrite_collection,
+            **kwargs,
+        )
+
+    @property
+    def vs_class(
+        self,
+    ) -> Type[VectorStoreExtension]:
+        return self._vector_stores[self.vector_store_type]
+
+
+# Set VectorStore Connector with the defined VectorStoreType
+VECTOR_STORE_CONNECTOR = VectorStoreConnector(vector_store_type=VECTOR_STORE_TYPE)

IvaSkriva/excersises

backend/apps/rag/vector_store/implementations/milvus.py
@@ -1,0 +1,89 @@
+from datetime import datetime
+from typing import Any, Type
+
+from apps.rag.vector_store.vector_store_extension import VectorStoreExtension
+from env import MILVUS_CONNECTION_URI
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+from langchain_milvus import Milvus as LangChainMilvus
+
+
+class Milvus(LangChainMilvus, VectorStoreExtension):
+    """
+    Milvus Interface that overrides Milvus PGVector Class with standardized attributes.
+
+    Args:
+        embedding_function (langchain_core.embeddings): Embedding Function with
+            LangChain interface.
+        collection_name (str): The collection name to use.
+        overwrite_collection (bool): Whether to overwrite current collection.
+        connection_args (dict[str, any]]): The connection args used for
+            this class comes in the form of a dict.
+        **kwargs (Any): Specific attributes linked to PGVector LangChain Class.
+    """
+
+    def __init__(
+        self,
+        embedding_function: Embeddings,
+        collection_name: str = "default",
+        overwrite_collection: bool = False,
+        connection_args: dict[str, any] = {"uri": MILVUS_CONNECTION_URI},
+        **kwargs: Any,
+    ):
+        self._overwrite_collection = overwrite_collection
+        self.collection_name = collection_name
+        self.embedding_function = embedding_function
+        # Avoid conflict of the same attribute appearing in kwargs with
+        # default value and what we add in the super().__init__ below
+        if "drop_old" in kwargs:
+            kwargs.pop("drop_old")
+        super().__init__(
+            collection_name=self.collection_name,
+            embedding_function=self.embedding_function,
+            drop_old=self._overwrite_collection,
+            connection_args=connection_args,
+            **kwargs,
+        )
+
+    def delete_collection(self) -> None:
+        """Delete collection."""
+        return self.col.drop()
+
+    @classmethod
+    def from_documents(
+        cls: Type[LangChainMilvus],
+        documents: list[Document],
+        embedding: Embeddings,
+        ## Modified args
+        connection_args: dict[str, any] = {"uri": MILVUS_CONNECTION_URI},
+        additional_metadata: dict = {},
+        ##
+        **kwargs: Any,
+    ) -> LangChainMilvus:
+        """Return VectorStore initialized from documents and embeddings.
+        This is a copy of the class method from_documents of LangChain Milvus
+        but with additional processes.
+
+        Args:
+            documents: List of Documents to add to the vectorstore.
+            embedding: Embedding function to use.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            VectorStore: VectorStore initialized from documents and embeddings.
+        """
+        texts = [d.page_content for d in documents]
+        metadatas = [{**d.metadata, **additional_metadata} for d in documents]
+        # Vectorstores do not like datetime formats
+        # for meta-data so convert them to string.
+        for metadata in metadatas:
+            for key, value in metadata.items():
+                if isinstance(value, datetime):
+                    metadata[key] = str(value)
+        return cls.from_texts(
+            texts,
+            embedding,
+            metadatas=metadatas,
+            connection_args=connection_args,
+            **kwargs,
+        )

backend/apps/rag/vector_store/vector_store_connector.py
@@ -1,0 +1,54 @@
+from typing import Any, Type
+
+from apps.rag.vector_store.implementations import (
+    Chroma,
+    Milvus,
+    PersistentChroma,
+    PGVector,
+)
+from apps.rag.vector_store.vector_store_extension import VectorStoreExtension
+from env import VECTOR_STORE_TYPE
+from langchain_core.embeddings import Embeddings
+
+
+class VectorStoreConnector:
+    """VectorStoreConnector class permits to use different VectorStores
+    by providing the VectorStore Type."""
+
+    _vector_stores = {
+        "chroma": Chroma,
+        "persistent_chroma": PersistentChroma,
+        "milvus": Milvus,
+        "pgvector": PGVector,
+    }
+
+    def __init__(self, vector_store_type: str = "persistent_chroma"):
+        if vector_store_type in self._vector_stores:
+            self.vector_store_type = vector_store_type
+        else:
+            raise ValueError(f"Unknown VectorStore type: {vector_store_type}")
+
+    def get_vs_collection(
+        self,
+        collection_name: str,
+        embedding_function: Embeddings,
+        overwrite_collection: bool = False,
+        **kwargs: Any,
+    ) -> VectorStoreExtension:
+        """Set VectorStore with the proper collection to be used."""
+        return self._vector_stores[self.vector_store_type](
+            collection_name=collection_name,
+            embedding_function=embedding_function,
+            overwrite_collection=overwrite_collection,
+            **kwargs,
+        )
+
+    @property
+    def vs_class(
+        self,
+    ) -> Type[VectorStoreExtension]:
+        return self._vector_stores[self.vector_store_type]
+
+
+# Set VectorStore Connector with the defined VectorStoreType
+VECTOR_STORE_CONNECTOR = VectorStoreConnector(vector_store_type=VECTOR_STORE_TYPE)